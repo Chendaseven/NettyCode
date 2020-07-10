d0e2f5adf2496909f45ed49f40b1ce0a47da887d
chenpeng42 <chenpeng42@meituan.com>
2020-07-10 16:24:41 +0800

    模拟客户端服务端双向通信

---
+++ src/main/java/com/seven/wechat/P3_bothWay/WeChatClient.java
@@ -0,0 +1,101 @@
+/**
+ * meituan.com Inc.
+ * Copyright (c) 2010-2020 All Rights Reserved.
+ */
+
+package com.seven.wechat.P3_bothWay;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.nio.charset.Charset;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * <p>
+ *
+ * </p>
+ * @author chenpeng
+ * @version :WeChatClient.java v1.0 2020/7/10 3:32 下午 chenpeng Exp $
+ */
+public class WeChatClient {
+
+    public static void main(String[] args) {
+        NioEventLoopGroup workGroup = new NioEventLoopGroup();
+        Bootstrap bootstrap = new Bootstrap();
+        bootstrap
+                //指定线程模型
+                .group(workGroup)
+                //指定IO类型为NIO
+                .channel(NioSocketChannel.class)
+                //IO处理逻辑
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel socketChannel) throws Exception {
+                        //逻辑处理器，负责向服务端写数据
+                        socketChannel.pipeline().addLast(new FirstClientHandler());
+                    }
+                });
+        //建立连接
+        bootstrap.connect("127.0.0.1",8000).addListener(future -> {
+                connect(bootstrap,"127.0.0.1",1001,5,5);
+        });
+    }
+
+    public static void connect(Bootstrap bootstrap,String host,int port,int retry,int MAX_RETRY){
+        bootstrap.connect(host,port).addListener(future -> {
+            if(future.isSuccess()){
+                System.out.println("连接成功！");
+            }else if(retry == 0){
+                System.err.println("重试次数已用完，放弃连接");
+            }else {
+                //计算第几次重连
+                int order = (MAX_RETRY - retry) + 1;
+                //重连的间隔
+                int delay = 1 << order;
+                System.err.println(new Date() + ":连接失败，第"+order+"次重连");
+                bootstrap.config().group().schedule(() -> connect(bootstrap,host,port,retry-1,MAX_RETRY),delay,
+                        TimeUnit.SECONDS);
+            }
+
+        });
+    }
+
+    static class FirstClientHandler extends ChannelInboundHandlerAdapter{
+        @Override
+        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+            System.out.println(new Date() + "：客户端写出数据");
+            //1.获取数据
+            ByteBuf buffer = getByteBuf(ctx);
+            //2.写数据
+            ctx.channel().writeAndFlush(buffer);
+        }
+
+        @Override
+        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+            ByteBuf byteBuf = (ByteBuf) msg;
+
+            System.out.println(new Date() + ": 客户端读到数据 -> " + byteBuf.toString(Charset.forName("utf-8")));
+        }
+
+        private ByteBuf getByteBuf(ChannelHandlerContext ctx){
+            //1.获取二进制抽象 ByteBuf
+            ByteBuf byteBuf = ctx.alloc().buffer();
+
+            //2.准备数据，指定utf-8
+            byte[] bytes = "超人你好".getBytes(Charset.forName("utf-8"));
+
+            //3.填充数据到ByteBuf
+            byteBuf.writeBytes(bytes);
+
+            return byteBuf;
+        }
+    }
+}
---
+++ src/main/java/com/seven/wechat/P3_bothWay/WeChatServer.java
@@ -0,0 +1,91 @@
+/**
+ * meituan.com Inc.
+ * Copyright (c) 2010-2020 All Rights Reserved.
+ */
+
+package com.seven.wechat.P3_bothWay;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
+
+import java.nio.charset.Charset;
+import java.util.Date;
+
+/**
+ * <p>
+ *
+ * </p>
+ * @author chenpeng
+ * @version :ChatServer.java v1.0 2020/7/10 3:21 下午 chenpeng Exp $
+ */
+public class WeChatServer {
+
+    public static void main(String[] args) {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workGroup = new NioEventLoopGroup();
+
+        //启动服务器
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        //服务器配置
+        serverBootstrap.group(bossGroup,workGroup)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel nioSocketChannel) throws Exception {
+                        nioSocketChannel.pipeline().addLast(new FirstServerHandler());
+                    }
+                });
+        serverBootstrap.bind(1000).addListener(new GenericFutureListener<Future<? super Void>>() {
+            @Override
+            public void operationComplete(Future<? super Void> future) throws Exception {
+                bind(serverBootstrap,1000);
+            }
+        });
+    }
+
+    public static void bind(final ServerBootstrap serverBootstrap,final int port){
+        serverBootstrap.bind(port).addListener(new GenericFutureListener<Future<? super Void>>() {
+            @Override
+            public void operationComplete(Future<? super Void> future) throws Exception {
+                if(future.isSuccess())
+                    System.out.println("端口["+port+"]端口绑定成功");
+                else{
+                    System.err.println("端口["+port+"]端口绑定失败");
+                    bind(serverBootstrap,port+1);
+                }
+            }
+        });
+    }
+
+    static class FirstServerHandler extends ChannelInboundHandlerAdapter {
+        @Override
+        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+            ByteBuf byteBuf = (ByteBuf) msg;
+            System.out.println(new Date() + "：服务端读取数据 ——>" + byteBuf.toString(Charset.forName("utf-8")));
+
+            //回复数据到client
+            System.out.println(new Date()+"：服务端写出数据");
+            ByteBuf out = getByteBuf(ctx);
+            ctx.channel().writeAndFlush(out);
+        }
+
+        private ByteBuf getByteBuf(ChannelHandlerContext ctx) {
+            byte[] bytes = "你好，欢迎关注我的微信公众号，《CSRobot》!".getBytes(Charset.forName("utf-8"));
+
+            ByteBuf buffer = ctx.alloc().buffer();
+
+            buffer.writeBytes(bytes);
+
+            return buffer;
+        }
+    }
+}
